ea4505f33de23b51f44f7cf3f5c0b42a61d21ff6
4ly-a <4ly.alcantara@gmail.com>
2021-03-22 10:49:58 -0300

    points

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber);
 const guesses = document.querySelector(".guesses");
+const points = document.querySelector(".points");
+
 const lastResult = document.querySelector(".lastResult");
 const card = document.querySelector(".info");
 const lowOrHi = document.querySelector(".lowOrHi");
@@ -10,30 +12,33 @@ const guessField = document.querySelector(".guessField");
 
 let guessCount = 1;
 let resetButton;
+let userPoints = 100;
 
 function checkGuess() {
   let userGuess = Number(guessField.value);
-
   guesses.textContent += userGuess + " ";
 
   if (userGuess === randomNumber) {
     lastResult.textContent = "Congratulations! You got it right!";
     card.style.backgroundColor = "green";
-    lowOrHi.textContent = "";
+    // lowOrHi.textContent = "";
     setGameOver();
   } else if (guessCount === 10) {
-    lastResult.textContent = "!!!GAME OVER!!!";
+    lastResult.textContent = "!!! GAME OVER !!!";
+    userPoints -= 10;
     setGameOver();
   } else {
+    userPoints -= 10;
     lastResult.textContent = "Wrong!";
     card.style.backgroundColor = "red";
     if (userGuess < randomNumber) {
-      lowOrHi.textContent = "Last guess was too low!";
+      lowOrHi.textContent = "Too low! ";
     } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = "Last guess was too high!";
+      lowOrHi.textContent = "Too high! ";
     }
   }
 
+  points.textContent = userPoints;
   guessCount++;
   guessField.value = "";
   guessField.focus();
@@ -56,6 +61,7 @@ function resetGame() {
 
   lastResult.style.backgroundColor = "white";
 
+  document.location.reload(true);
   randomNumber = Math.floor(Math.random() * 100) + 1;
   console.log(randomNumber);
 }
@@ -67,7 +73,6 @@ function setGameOver() {
   resetButton.textContent = "Start new Game";
   resetButton.className = "resetButton";
   document.body.append(resetButton);
-
   resetButton.addEventListener("click", resetGame);
 }
 
